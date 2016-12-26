65d9f697bf885021f336f719c159e8fe94ac2b37
Alex <alexander.stoyakin@gmail.com>
2016-12-26 19:52:11 +0200

    Added territories methods

---
+++ src/route4me/route4me.cpp
@@ -0,0 +1,6 @@
+#include "route4me.h"
+
+
+Route4me::Route4me()
+{
+}
---
+++ src/route4me/route4me.h
@@ -0,0 +1,13 @@
+#ifndef ROUTE4ME_H
+#define ROUTE4ME_H
+
+#include "route4me_global.h"
+
+class ROUTE4MESHARED_EXPORT Route4me
+{
+
+public:
+    Route4me();
+};
+
+#endif // ROUTE4ME_H
